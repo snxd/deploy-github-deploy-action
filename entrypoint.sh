8295979d440ac3e2d7e570f27b67daadd1c2da6d
Bo Zimmerman <bo@zimmers.net>
2022-05-13 14:34:06 -0700

    pip worked, but python not found?  guessing wildly at this point.

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,6 +46,6 @@ cd solsta_work
 # Install any missing deploy script dependencies
 pip install -r requirements.txt
 # Download the latest SSN Console Tools if necessary
-if [ ! -d "solsta_console" ]; then python direct_get.py --overwrite --version="$INPUT_CONSOLE_VERSION" --target_directory=solsta_console --console_credentials=client_credentials.json ; fi
+if [ ! -d "solsta_console" ]; then python3 direct_get.py --overwrite --version="$INPUT_CONSOLE_VERSION" --target_directory=solsta_console --console_credentials=client_credentials.json ; fi
 # Run the script that creates a new release and deploys it
-python release_deploy.py --debug --console_credentials=client_credentials.json --console_directory=solsta_console --product_name="$INPUT_TARGET_PRODUCT" --env_name="$INPUT_TARGET_ENVIRONMENT" --repository_name="$INPUT_TARGET_REPOSITORY" --source="$INPUT_WORKING_DIRECTORY"
+python3 release_deploy.py --debug --console_credentials=client_credentials.json --console_directory=solsta_console --product_name="$INPUT_TARGET_PRODUCT" --env_name="$INPUT_TARGET_ENVIRONMENT" --repository_name="$INPUT_TARGET_REPOSITORY" --source="$INPUT_WORKING_DIRECTORY"
